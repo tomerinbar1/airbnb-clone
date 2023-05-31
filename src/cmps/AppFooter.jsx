b3b4a841c34356008943ba14bd867f1f686b6197
Ilan Idov <cafe.ilanidov@gmail.com>
2023-05-31 23:38:24 +0300

    connection check

--- src/cmps/AppFooter.jsx
+++ src/cmps/AppFooter.jsx
@@ -1,7 +1,7 @@
 export const AppFooter = () => {
     return (
         <footer className="app-footer">
-            <h1>Footer</h1>
+            <h1>Hello from Ilan</h1>
         </footer>
     )
 }
