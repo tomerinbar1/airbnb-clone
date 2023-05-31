8d88b2cd22bfa518504679dacb5c5c295068bf04
tomerinbar1 <tomerinbar1@gmail.com>
2023-05-31 23:37:43 +0300

    add basic structure and cmps

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom"
+
 export const HomePage = () => {
   return (
     <div>
       <h1>Home</h1>
+      <Link to="/stayId">Stay Details</Link>
     </div>
   )
 }
---
+++ src/pages/StayDetails.jsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom'
+
+export const StayDetails = () => {
+  return (
+    <section className="stay-details">
+      <h1>Stay Details</h1>
+      <Link to="/">Home</Link>
+    </section>
+  )
+}
