2b22db38447d2191bca059197b43958395ca0ae2
Ilan Idov <cafe.ilanidov@gmail.com>
2023-06-01 18:05:47 +0300

    updating

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -13,10 +13,8 @@ export const AppHeader = () => {
     <header className="app-header flex">
 
       <h1 onClick={onLogoClick}>Air-BNB</h1>
-
       <SearchBar />
       <Login />
-
       {/* <div className="user-options">       for future i18? and more, dont forget to set style props to parent div
         <Login />
       </div> */}
--- src/cmps/Login.jsx
+++ src/cmps/Login.jsx
@@ -5,11 +5,12 @@ export function Login() {
 
 
 
-
     return (
-        <div className="login">
-            
+        <div className="login-header-btn">
+            <div className="login-burger"></div>
+            <div className="login-user-pic-container">
+                {/* <img className="login-user-pic" src="" alt="" /> */}
+            </div>
         </div>
     )
-
 }
