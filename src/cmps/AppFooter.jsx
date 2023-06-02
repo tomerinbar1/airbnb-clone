85d68eb712f23dee7a74f6e8039c05138aae997d
Shira Cohen <shira140@gmail.com>
2023-06-02 18:50:13 +0300

    layout update

--- src/cmps/AppFooter.jsx
+++ src/cmps/AppFooter.jsx
@@ -1,7 +1,7 @@
 export const AppFooter = () => {
     return (
         <footer className="app-footer full">
-            <h1>Footer</h1>
+            {/* <h1>Footer</h1> */}
         </footer>
     )
 }
