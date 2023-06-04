11d5e49fcbb76dfef05ff242a8eb264775c0b3a6
Shira Cohen <shira140@gmail.com>
2023-06-05 00:32:14 +0300

    basic crudl

--- src/services/event-bus.service.js
+++ src/services/event-bus.service.js
@@ -1,16 +1,14 @@
+export const SHOW_MSG = 'show-msg'
+
 function createEventEmitter() {
     const listenersMap = {}
-    // Trick for DEBUG
-    window.mapmap = listenersMap
     return {
-
         on(evName, listener){
             listenersMap[evName] = (listenersMap[evName])? [...listenersMap[evName], listener] : [listener]
             return ()=>{
                 listenersMap[evName] = listenersMap[evName].filter(func => func !== listener)
             }
         },
-
         emit(evName, data) {
             if (!listenersMap[evName]) return
             listenersMap[evName].forEach(listener => listener(data))
@@ -18,14 +16,18 @@ function createEventEmitter() {
     }
 }
 
-export const eventBusService = createEventEmitter()
+export const eventBus = createEventEmitter()
 
 export function showUserMsg(msg) {
-    eventBusService.emit('show-user-msg', msg)
+    eventBus.emit(SHOW_MSG, msg)
 }
+
 export function showSuccessMsg(txt) {
     showUserMsg({txt, type: 'success'})
 }
 export function showErrorMsg(txt) {
     showUserMsg({txt, type: 'error'})
 }
+
+window.showUserMsg = showUserMsg
+
