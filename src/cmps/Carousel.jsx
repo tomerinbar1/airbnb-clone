8ec816c27d14282e56a9110de040fb4ad6d4642e
Shira Cohen <shira140@gmail.com>
2023-06-06 02:06:07 +0300

    category carousel

--- src/cmps/Carousel.jsx
+++ src/cmps/Carousel.jsx
@@ -7,12 +7,20 @@ export function Carousel({ stay }) {
   const updateIndex = (newIndex, ev) => {
     ev.preventDefault()
     if (newIndex < 0) {
-      newIndex = stay.imgUrls.length - 1
-    } else if (newIndex >= stay.imgUrls.length) {
       newIndex = 0
+      // newIndex = stay.imgUrls.length - 1
+    } else if (newIndex >= stay.imgUrls.length) {
+      // newIndex = 0
+      newIndex = stay.imgUrls.length - 1
+
     }
     setActiveIndex(newIndex)
   }
+
+ 
+  const dynClassBack = (activeIndex>=1) ? 'show-arrow' : 'hide-arrow'
+  const dynClassForward = (activeIndex >= (stay.imgUrls.length - 1)) ? 'hide-arrow':'show-arrow' 
+
 
   return (
     <div className="carousel">
@@ -28,23 +36,29 @@ export function Carousel({ stay }) {
       </div>
 
       <div className="carousel-arrows">
+
+      <section className= {dynClassBack}>
         <button
-          className="button-arrow"
+          className="button-arrow-back"
+          // className="button-arrow"
           onClick={(ev) => {
             updateIndex(activeIndex - 1, ev)
           }}
         >
-          <span className="material-symbols-outlined">arrow_back_ios</span>
+          <span className="material-symbols-outlined arrow-back">arrow_back_ios</span>
         </button>
+        </section>
 
+       <section className={dynClassForward}>
         <button
-          className="button-arrow"
+          className= "button-arrow-forward"
           onClick={(ev) => {
             updateIndex(activeIndex + 1, ev)
           }}
         >
           <span className="material-symbols-outlined">arrow_forward_ios</span>
         </button>
+      </section>
       </div>
 
       <div className="indicators">
--- src/cmps/CarouselItem.jsx
+++ src/cmps/CarouselItem.jsx
@@ -7,3 +7,5 @@ export const CarouselItem = ({ imageUrl, width }) => {
     </div>
   )
 }
+
+
