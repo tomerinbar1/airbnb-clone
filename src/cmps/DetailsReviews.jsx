cfddf8b6650fe87fb6e8373e46a7bf41726fe00e
tomerinbar1 <tomerinbar1@gmail.com>
2023-06-05 16:47:22 +0300

    Change the fonts, remove the font-weight properties

--- src/cmps/DetailsReviews.jsx
+++ src/cmps/DetailsReviews.jsx
@@ -1,25 +1,38 @@
+import { StayReviewsStat } from './StayReviewsStat'
+
 export const DetailsReviews = ({ reviews }) => {
-    return(
-        <div className="reviews-details">
-        <div className="rating-details">
-          <span>⭐️4.95</span>
-          <span>346 reviews</span>
+  //   console.log('reviews', reviews)
+
+  const averageRates = reviews => {
+    reviews.map(review => {
+      const { rate } = review
+      const rateTypes = Object.keys(rate)
+      // console.log('rateTypes', rateTypes)
+      const rateValues = Object.values(rate)
+      // console.log('rateValues', rateValues)
+    })
+  }
+
+  averageRates(reviews)
+
+  return (
+    <div className="reviews-details-wrapper">
+      <div className="rating-details">
+        <StayReviewsStat reviews={reviews} />
+      </div>
+      <div className="reviews-avg-data">
+        <div className="reviews-avg-data-left">
+          <ul></ul>
         </div>
-        <div className="reviews-avg-data">
-          <ul>
-            <li>Cleanliness</li>
-            <li>Accuracy</li>
-            <li>Communication</li>
-            <li>Location</li>
-            <li>Check-in</li>
-            <li>Value</li>
-          </ul>
-        </div>
-        <div className="reviews-wrapper">
-          <div className="reviews-left">review-cards render here</div>
-          <div className="reviews-right">review-cards render here</div>
-          <button>Show all 346 reviews</button>
+        <div className="reviews-avg-data-right">
+          <ul></ul>
         </div>
       </div>
-    )
+      <div className="reviews-wrapper">
+        <div className="reviews-left">review-cards render here</div>
+        <div className="reviews-right">review-cards render here</div>
+        <button>Show all 346 reviews</button>
+      </div>
+    </div>
+  )
 }
