9778e8a23695fe64ba195b59f78da74aff70b15a
Shira Cohen <shira140@gmail.com>
2023-06-06 19:27:21 +0300

    labels carousel

--- src/cmps/CarouselLabels.jsx
+++ src/cmps/CarouselLabels.jsx
@@ -1,27 +1,35 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { CarouselItemLabels } from "./CarouselItemLabels"
 
 export function CarouselLabels({ categories }) {
   const [activeIndex, setActiveIndex] = useState(0)
+
+ 
+  // console.log(categories)
+  const pagesCalc = (categories) => {
+    const iconsPerPage = 15
+    let pages = Array.from({ length: Math.ceil(categories.length / iconsPerPage) }, (_, index) =>
+      categories.slice(index * iconsPerPage, (index + 1) * iconsPerPage)
+    )
+    return pages
+  }
+
+
+
+  const pages = pagesCalc(categories)
+
   const updateIndex = (newIndex, ev) => {
     ev.preventDefault()
     if (newIndex < 0) {
-      newIndex = categories.length - 1
-      // newIndex = stay.imgUrls.length - 1
-    } else if (newIndex >= categories.length) {
-      // newIndex = 0
+      newIndex = pages.length - 1
+    } else if (newIndex >= pages.length) {
       newIndex = 0
-
     }
     setActiveIndex(newIndex)
   }
 
-
-  // const dynClassBack = (activeIndex>=1) ? 'show-arrow' : 'hide-arrow'
-  // const dynClassForward = (activeIndex >= (stay.imgUrls.length - 1)) ? 'hide-arrow':'show-arrow' 
-
-
   return (
+
     <div className="carousel-labels">
       <div
         className="inner-labels"
@@ -29,21 +37,23 @@ export function CarouselLabels({ categories }) {
           transform: `translate(-${activeIndex * 100}%)`
         }}
       >
-        {categories.map((category) => {
-            // console.log(category.url)
+        {pages.map((page, index) => {
+          return (
+            <CarouselItemLabels key={index} page={page} width={"100%"} />
+          )
+        })}
+      </div>
 
+      {/* {categories.map((category) => {
           return (
             <span className="category-carousel-item">
               <CarouselItemLabels key={category.url} categoryUrl={category.url} width={"100%"} />
               <p className='category-name'>{category.name}</p>
             </span>
           )
-        })}
-      </div>
+        })} */}
 
       <div className="carousel-labels-arrows">
-
-        {/* <section className={dynClassBack}> */}
         <button
           className="button-arrow-back-labels"
           onClick={(ev) => {
@@ -52,9 +62,7 @@ export function CarouselLabels({ categories }) {
         >
           <span className="material-symbols-outlined arrow-back">arrow_back_ios</span>
         </button>
-        {/* </section> */}
 
-        {/* <section className={dynClassForward}> */}
         <button
           className="button-arrow-forward-labels"
           onClick={(ev) => {
@@ -63,7 +71,6 @@ export function CarouselLabels({ categories }) {
         >
           <span className="material-symbols-outlined">arrow_forward_ios</span>
         </button>
-        {/* </section> */}
       </div>
 
 
