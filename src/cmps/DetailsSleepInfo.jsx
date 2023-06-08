051ac5b50ab5c16ff1edd141f754862e0ab4f3d4
tomerinbar1 <tomerinbar1@gmail.com>
2023-06-08 11:38:10 +0300

    setup mongoDB Atlas

--- src/cmps/DetailsSleepInfo.jsx
+++ src/cmps/DetailsSleepInfo.jsx
@@ -1,11 +1,11 @@
 export const DetailsSleepInfo = ({ imgUrl, onOpenModal }) => {
     
   return(
-        <div onClick={() => onOpenModal()} className="sleep-details">
+        <section onClick={() => onOpenModal()} className="sleep-details">
         <h1>Where you'll sleep</h1>
         <img src={imgUrl} alt="bed-img" />
         <h2>Bedroom</h2>
         <p>1 queen bed</p>
-      </div>
+      </section>
     )
 }
