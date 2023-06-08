e727c73c87bb70520d3c801ef63b361e11201263
Shira Cohen <shira140@gmail.com>
2023-06-08 13:30:52 +0300

    basic calander

---
+++ src/cmps/user/orders/DatePicker.jsx
@@ -0,0 +1,56 @@
+import 'react-date-range/dist/styles.css' // main css file
+import 'react-date-range/dist/theme/default.css' // theme css file
+// import '../../../assets/styles/cmps/_OrderModal.scss'
+import { DateRange } from 'react-date-range'
+import { useState } from 'react'
+
+
+
+export function DatePicker({ onSetField }) {
+    const checkIn = new Date()
+    const checkOut = new Date()
+    const monthsToShow = 2
+
+
+    // checkIn = checkIn || new Date()
+    // checkOut = checkOut || new Date()
+
+
+
+    const [range, setRange] = useState([
+        {
+            startDate: checkIn,
+            endDate: checkOut,
+            key: 'selection'
+        }
+    ])
+
+    function handleChange(item) {
+        const selection = item.selection
+        // console.log(selection)
+        setRange([selection])
+        if (selection.startDate === selection.endDate) {
+            onSetField('checkIn', selection.startDate)
+        } else {
+            onSetField('checkOut', selection.endDate)
+        }
+    }
+
+
+    return (
+        <DateRange
+            editableDateInputs={true}
+            onChange={handleChange}
+            moveRangeOnFirstSelection={false}
+            ranges={range}
+            months={monthsToShow}
+            direction="horizontal"
+            rangeColors={['black']}
+            showDateDisplay={false}
+            showMonthAndYearPickers={false}
+            className="calendar-element"
+        // disabledDates=[]
+
+        />
+    )
+}
---
+++ src/cmps/user/orders/StayDetailsOrder.jsx
@@ -0,0 +1,124 @@
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DatePicker } from './DatePicker';
+import { useState } from 'react'
+import { useSearchParams, useNavigate } from 'react-router-dom'
+
+
+
+
+export function StayDetailsOrder({ stay, setOpenTab, openTab }) {
+    const [searchParams, setSearchParams] = useSearchParams()
+
+
+
+    const orderParams = {
+        checkIn: searchParams.get('checkIn')
+            ? new Date(+searchParams.get('checkIn'))
+            : '',
+        checkOut: searchParams.get('checkOut')
+            ? new Date(+searchParams.get('checkOut'))
+            : '',
+        guests: {
+            adults: +searchParams.get('adults') || 1,
+            children: +searchParams.get('children') || 0,
+            infants: +searchParams.get('infants') || 0,
+            pets: +searchParams.get('pets') || 0,
+        },
+    }
+
+
+
+
+
+
+
+    function onSetField(field, value) {
+        // console.log(searchParams)
+        // if (field === 'guests') {
+        //   searchParams.set('adults', value.adults)
+        //   searchParams.set('children', value.children)
+        //   searchParams.set('infants', value.infants)
+        //   searchParams.set('pets', value.pets)
+        // }
+        if (field === 'checkIn' || field === 'checkOut') {
+            if (value) {
+                value = value.getTime()
+            }
+            searchParams.set(field, value)
+        }
+        setSearchParams(searchParams)
+    }
+
+    console.log(stay)
+    return (
+        <section className='order-modal'>
+            <section className='user-prefs'>
+                <section className='order-modal-header'>
+                    <span>{stay.price}</span>
+                    <span>night</span>
+                    <span>rate</span>
+                    <span>1 review</span>
+                </section>
+
+                <section className='picker-container'>
+                    {(openTab === 'checkIn' || openTab === 'checkOut') && (
+                        <section className='date-picker-container'>
+
+                            {/* {(orderParams.checkIn && orderParams.checkOut) ? (<h4>{utilService.totalDays(orderParams.checkIn, orderParams.checkOut)} nights</h4>) : <h4>Select Dates</h4>} */}
+
+                            {/* {(orderParams.checkIn && orderParams.checkOut) ? ( <h5>{utilService.ShortFormattedDate(orderParams.checkIn)}-{utilService.ShortFormattedDate(orderParams.checkOut)}</h5>) : <h5>Minimum nights: 2 days</h5>} */}
+
+
+
+                            <DatePicker
+                                // checkIn={orderParams.checkIn}
+                                // checkOut={orderParams.checkOut}
+                                onSetField={onSetField}
+                                className='date-picker'
+                                style={{
+                                    backgroundColor: 'black',
+                                }}
+                            />
+                            <section className='open-picker-btns'>
+                                <button className='clear-open-picker-btn' onClick={() => { onSetField('checkIn', ''); onSetField('checkOut', '') }}>Clear dates</button>
+                                <button className='close-open-picker-btn' onClick={() => setOpenTab('')}>Close</button>
+                            </section>
+                        </section>
+
+                    )}
+                    <section className='picker-btns' >
+                    <button
+                        onClick={() => setOpenTab('checkIn')}
+                        className='clean-button check-in picker'>
+                        <div className='order-heading'>Check-In</div>
+                        {/* <div className='order-sub-heading'>{checkInSubHeading}</div> */}
+                    </button>
+
+                    <button
+                        onClick={() => setOpenTab('checkOut')}
+                        className='clean-button check-out picker'>
+                        <div className='order-heading'>Check-Out</div>
+                        {/* <div className='order-sub-heading'>{checkOutSubHeading}</div> */}
+                    </button>
+                    </section>
+                </section>
+
+                <button className='reserve-btn'>Reserve</button>
+            </section>
+
+            <section className='chrage-msg'>
+                You won't be charged yet
+            </section>
+            <section className='price-summary'>
+                <p> $ * nights</p>
+                <p>Cleaning fee</p>
+                <p>Airbnb service fee</p>
+                <p>Total</p>
+            </section>
+
+        </section>
+
+
+    )
+}
