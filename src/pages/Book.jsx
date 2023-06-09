668caf7f647d09a2f5cc85ad96ac020fa3132228
tomerinbar1 <tomerinbar1@gmail.com>
2023-06-09 18:21:58 +0300

    Book page WIP

--- src/pages/Book.jsx
+++ src/pages/Book.jsx
@@ -1,54 +1,72 @@
 export const Book = () => {
   return (
-    <div className="book-wrapper">
-      <button className="book-return-btn">{'\u003C'}</button>
-      <div className="book-details-header">
-        <h1>Request to book</h1>
-      </div>
-      <section className="book-details">
-        <h2>Your trip</h2>
-        <div className="book-details-trip-info">
-          <div className="book-details-trip-date">
-            <h3>Dates</h3>
-            <p>Jun 10 - 15</p> // TODO: Replace with actual dates
-            <div className="book-details-edit">
-              <a href="#">Edit</a>
-            </div>
-          </div>
-          <div className="book-details-trip-guests">
-            <h3>Guests</h3>
-            <h3>1 guest</h3> // TODO: Replace with actual guests
-            <div className="book-details-edit">
-              <a href="#">Edit</a>
-            </div>
-          </div>
+    <div className="book-page-wrapper">
+      <section className="book-details-wrapper">
+        <div className="book-details-header">
+          <h1>Request to book</h1>
+          <button className="book-return-btn">{'\u003C'}</button>
         </div>
-        <div className="book-payments-form">
-          <form action=""></form>
-        </div>
-        <div className="book-required">
-          <div className="message">
-            <h3>Message the Host</h3>
-            <p>
-              Let the host know why you’re travelling and when you’ll check in.
-            </p>
-            <div className="book-add-message">
-              <button className="book-add-btn">Add</button>
+        <div className="book-details">
+          <h2>Your trip</h2>
+          <div className="book-details-trip-info">
+            <div className="book-details-trip-date">
+              <div className="date-info">
+                <h3>Dates</h3>
+                <p>Jun 10 - 15</p>
+              </div>
+              <div className="book-details-edit">
+                <a href="#">Edit</a>
+              </div>
+            </div>
+            <div className="book-details-trip-guests">
+              <div className="guest-info">
+                <h3>Guests</h3>
+                <p>1 guest</p>
+              </div>
+              <div className="book-details-edit">
+                <a href="#">Edit</a>
+              </div>
             </div>
           </div>
-          <div className="phone-number">
-            <h3>Phone number</h3>
-            <p>Add and confirm your phone number to get trip updates.</p>
-            <div className="book-add-message">
-            <button className="book-add-btn">Add</button>
-            </div>
-          </div>
-        </div>
-        <div className="book-terms"></div>
-      </section>
 
-      <section className="price-details">
-        // TODO: Replace with price details modal
+          <hr className="custom-hr" />
+
+          <div className="book-payments-form">
+            <h2>Choose how to pay</h2>
+            <form action=""></form>
+          </div>
+
+          <hr className="custom-hr" />
+
+        
+          <div className="book-required">
+            <div className="required-info">
+              <div className="message">
+                <h3>Message the Host</h3>
+                <p>
+                  Let the host know why you’re travelling and when you’ll check
+                  in.
+                </p>
+                <div className="book-add-message">
+                  <button className="book-add-btn">Add</button>
+                </div>
+              </div>
+              <div className="phone-number">
+                <h3>Phone number</h3>
+                <p>Add and confirm your phone number to get trip updates.</p>
+                <div className="book-add-message">
+                  <button className="book-add-btn">Add</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="book-terms"></div>
+        </div>
+      </section>
+      <section className="book-summary-details">
+        <div className="price-details">
+          // TODO: Replace with price details modal
+        </div>
       </section>
     </div>
   )
--- src/store/stay.reducer.js
+++ src/store/stay.reducer.js
@@ -1,3 +1,4 @@
+import { storageService } from '../services/storage.service'
 export const SET_STAYS = 'SET_STAYS'
 export const REMOVE_STAY = 'REMOVE_STAY'
 export const ADD_STAY = 'ADD_STAY'
@@ -7,44 +8,46 @@ export const REQUEST_STAYS = 'REQUEST_STAYS'
 // export const UNDO_REMOVE_STAY = 'UNDO_REMOVE_STAY'
 
 const initialState = {
-    stays: [],
-    isLoading: false,
-    // lastRemovedStay: null
+  stays: [],
+  isLoading: false,
+  // lastRemovedStay: null
 }
 
-
 export function stayReducer(state = initialState, action) {
-    // console.log('action', action)
-    let stays
-    // let shoppingStayt
+  // console.log('action', action)
+  let stays
+  // let shoppingStayt
 
-    switch (action.type) {
-        // Stays
-        case SET_IS_LOADING:
-            return { ...state, isLoading: action.isLoading }
-        case REQUEST_STAYS:
-            // get stay from local storage
-            // const stays = JSON.parse(localStorage.getItem('stays'))
-            if (stays && stays.length) {
-                return { ...state, stays, isLoading: false }
-            }
-            return { ...state, isLoading: true }
-        case SET_STAYS:
-            // set localstorage
-            // localStorage.setItem('stays', JSON.stringify(action.stays))
-            return { ...state, stays: action.stays }
-        case REMOVE_STAY:
-            stays = state.stays.filter(c => c._id !== action.stayId)
-            return { ...state, stays }
-        case ADD_STAY:
-            stays = [...state.stays, action.stay]
-            return { ...state, stays }
-        case UPDATE_STAY:
-            stays = state.stays.map(stay => stay._id === action.stay._id ? action.stay : stay)
-            return { ...state, stays }
+  switch (action.type) {
+    // Stays
+    case SET_IS_LOADING:
+      return { ...state, isLoading: action.isLoading }
+    case REQUEST_STAYS:
+      const staysFromCache = storageService.loadFromStorage('stays')
+      if (staysFromCache) {
+        return { ...state, stays: staysFromCache, isLoading: false }
+      }
 
+      if (stays && stays.length) {
+        return { ...state, stays, isLoading: false }
+      }
+      return { ...state, isLoading: true }
+    case SET_STAYS:
+        storageService.saveToStorage('stays', action.stays)
+      return { ...state, stays: action.stays }
+    case REMOVE_STAY:
+      stays = state.stays.filter(c => c._id !== action.stayId)
+      return { ...state, stays }
+    case ADD_STAY:
+      stays = [...state.stays, action.stay]
+      return { ...state, stays }
+    case UPDATE_STAY:
+      stays = state.stays.map(stay =>
+        stay._id === action.stay._id ? action.stay : stay
+      )
+      return { ...state, stays }
 
-        default:
-            return state
-    }
+    default:
+      return state
+  }
 }
