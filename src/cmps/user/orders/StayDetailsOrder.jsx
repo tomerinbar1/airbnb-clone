47f6ac9f388cdffe187b5521d8d1f8fa99593bb6
Shira Cohen <shira140@gmail.com>
2023-06-11 11:00:52 +0300

    order modal update

--- src/cmps/user/orders/StayDetailsOrder.jsx
+++ src/cmps/user/orders/StayDetailsOrder.jsx
@@ -173,8 +173,10 @@ export function StayDetailsOrder({ stay, setOpenTab, openTab }) {
 
                 <section className='guests'>
                     <section className='guests-close'>
-                        <button onClick={() => (openTab === 'guests' ? setOpenTab(null) : setOpenTab('guests'))} className="guests-btn">GUESTS
+                        <button onClick={() => (openTab === 'guests' ? setOpenTab(null) : setOpenTab('guests'))} className="guests-btn">
                             {getGuestsCount()}
+                            <span className='guests-count'>GUESTS</span>
+                            
                         </button>
                         {openTab === 'guests' && (
                             <section className="guests-open">
